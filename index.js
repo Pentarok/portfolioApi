5309922dfd0ab01202ecbba3f7538c8da96a6f24
Pentarok <167968478+Pentarok@users.noreply.github.com>
2024-10-04 22:14:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:'https://portfolio-8ltz.vercel.app',
     methods:["GET","POST"],
     credentials:true
 }))
