8a306027f611321f8c4ad054e5ad33f9fab3e462
Pentarok <167968478+Pentarok@users.noreply.github.com>
2024-11-06 15:11:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-    origin:process.env.FRONT_END_URL,
-    methods:["GET","POST"],
-    credentials:true
-}))
+  origin: process.env.FRONT_END_URL,
+  methods: ["GET", "POST", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
